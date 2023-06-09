16bd484db656cce54de2cabedfde235c995a8e44
Manoel Alves <manoelskty@gmail.com>
2023-06-09 20:02:58 -0300

    Programa que fala nome e idade

---
+++ ex_chat.py
@@ -0,0 +1,21 @@
+# Valor da renda anual de João
+# renda_anual = float(input("Digite o valor da renda anual de João: "))
+
+# Verifica se João paga imposto
+# if renda_anual <= 30000:
+# print("João está inseto de pagar imposto.")
+
+# else:
+# imposto = renda_anual * 0.15
+# print("João deve pagar R$ {:.2f} de imposto.".format(imposto))
+
+
+# Solicita o nome da pessoa
+nome = input("Digite seu nome: ")
+
+# Solicita a idade da pessoa
+idade = int(input("Digite sua idade: "))
+
+# Imprime o nome e a idade da pessoa com placeholders utilizando (f) como formato de texto
+print(f"Seu Nome é : {nome}")
+print(f"Sua idade é : {idade}")
