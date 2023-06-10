8050a886e48c00a4bbcabf0954bb2c4970a40433
Manoel Alves <manoelskty@gmail.com>
2023-06-09 23:58:50 -0300

    inserindo_imposto

--- imposto_joão.py
+++ imposto_joão.py
@@ -3,4 +3,4 @@ renda_anual = float(input("Digite o valor da renda anual de João: "))
 
 # Verifica se João paga imposto
 if renda_anual <= 30000:
-    print()
+    print("João está inseto de pagar imposto.")
