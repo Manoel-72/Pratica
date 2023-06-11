9d7c28cf2a209caa0017813c78575740d1d22133
Manoel Alves <manoelskty@gmail.com>
2023-06-11 11:23:32 -0300

    imposto_concluido

--- imposto_joão.py
+++ imposto_joão.py
@@ -4,3 +4,8 @@ renda_anual = float(input("Digite o valor da renda anual de João: "))
 # Verifica se João paga imposto
 if renda_anual <= 30000:
     print("João está inseto de pagar imposto.")
+
+else:
+    imposto = renda_anual * 0.15
+
+    print("João deve paga R$ {:.2f} de imposto.".format(imposto))
