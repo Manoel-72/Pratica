0759969d543e97418dea89caf3600dfeb16889c2
Manoel Alves <manoelskty@gmail.com>
2023-06-13 01:06:58 -0300

    Exercicio de media de numero de 0 ate 100

---
+++ 100 Exercicios/Exercicio_5.py
@@ -0,0 +1,18 @@
+"""Exercicio intermediario de inicainte Primeiro"""
+
+# Solicita ao usuario a quantidade de números
+_quant_ = int(input("Digite a quantidade de números: "))
+
+# Cria uma lista vazia para armazenar os números
+numeros = []
+
+# Solicita ao usuário os números e adiciona-os a lista
+for i in range(_quant_):
+    numero = float(input(f"Digite o número {i+1}: "))
+    numeros.append(numero)
+
+# Calcula a media
+media = sum(numeros) / len(numeros)
+
+# Exibe o resultado utilizando f-string
+print(f"A media dos números é: {media}")
